32e16f2d2d3657e77d2f8ad53947054185fb7ff3
IvanTurbin <ivan.turbin@gmail.com>
2019-02-20 01:03:19 +0200

    Update misc.py

--- commands/misc.py
+++ commands/misc.py
@@ -11,6 +11,15 @@ class Misc(object):
         basewrapper.Base().info_logger(f"{ctx.message.author} - Ping!")
         await self.client.say(f"{ctx.message.author.mention} Pong!")
 
+    @commands.command(pass_context=True)
+    async def clear(self, ctx: commands.Context, *, number):
+        number = int(number)  # Converting the amount of messages to delete to an integer
+        counter = 0
+        async for x in self.client.logs_from(ctx.message.channel, limit=number):
+            if counter < number:
+                await self.client.delete_message(x)
+                counter += 1
+
 
 def setup(client: commands.Bot):
     client.add_cog(Misc(client))
