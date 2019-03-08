bcaf5c95093c01d1285f0dd1fe7dd0271d32cbcf
Ivan Turbin <ivan.turbin@f-secure.com>
2019-03-08 13:41:30 +0200

    added try

--- commands/misc.py
+++ commands/misc.py
@@ -32,38 +32,38 @@ class Misc(object):
 
     @commands.command(pass_context=True)
     async def steam(self, ctx: commands.Context, *, name):
-        steam_api = basewrapper.Base().get_config_vars("steamapi")
-        url = r"http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/"
-        payload = {"key": steam_api, "vanityurl": name}
+        try:
+            steam_api = basewrapper.Base().get_config_vars("steamapi")
+            url = r"http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/"
+            payload = {"key": steam_api, "vanityurl": name}
 
-        r = requests.get(url, params=payload)
-        steam_comunity_id = r.json()["response"]["steamid"]
-        basewrapper.Base().info_logger(steam_comunity_id)
+            r = requests.get(url, params=payload)
+            steam_comunity_id = r.json()["response"]["steamid"]
+            basewrapper.Base().info_logger(steam_comunity_id)
 
-        #Get steam id through community ID obtained above
+            #Get steam id through community ID obtained above
 
-        print(steam_comunity_id)
+            print(steam_comunity_id)
 
-        url = r"https://api.steamid.uk/request.php"
-        steam_id = basewrapper.Base().get_config_vars("steamid")
-        payload = {"api": steam_id, "player": steam_comunity_id, "request": 36, "format": "json"}
+            url = r"https://api.steamid.uk/request.php"
+            steam_id = basewrapper.Base().get_config_vars("steamid")
+            payload = {"api": steam_id, "player": steam_comunity_id, "request": 36, "format": "json"}
 
-        r = requests.get(url, params=payload)
-        print(r.content)
-        steamdata = r.json()
+            r = requests.get(url, params=payload)
+            steamdata = r.json()
 
-        basewrapper.Base().info_logger(f"Searched for {name} steamid: {steamdata}")
-        await self.client.say(f"{ctx.message.author.mention} Steam info for {name}: \n"
-                              f"```Steam playername: {steamdata['profile']['playername']} \n"
-                              f"Steam Profile detailed: {steamdata['profile']['steamidurl']} \n"
-                              f"Steam Profile: https://steamcommunity.com/id/{name} \n"
-                              f"SteamID: {steamdata['profile']['steamid']} \n"
-                              f"SteamID3: {steamdata['profile']['steam3']} \n"
-                              f"Bans - Vac {steamdata['profile_status']['vac']} - Trade {steamdata['profile_status']['tradeban']} - Community Ban {steamdata['profile_status']['communityban']}```")
-        """
+            basewrapper.Base().info_logger(f"Searched for {name} steamid: {steamdata}")
+            await self.client.say(f"{ctx.message.author.mention} Steam info for {name}: \n"
+                                  f"```Steam playername: {steamdata['profile']['playername']} \n"
+                                  f"Steam Profile detailed: {steamdata['profile']['steamidurl']} \n"
+                                  f"Steam Profile: https://steamcommunity.com/id/{name} \n"
+                                  f"SteamID: {steamdata['profile']['steamid']} \n"
+                                  f"SteamID3: {steamdata['profile']['steam3']} \n"
+                                  f"Bans - Vac {steamdata['profile_status']['vac']} - Trade {steamdata['profile_status']['tradeban']} - Community Ban {steamdata['profile_status']['communityban']}```")
+
         except Exception as e:
             await self.client.say(f"{ctx.message.author.mention} No account found!")
-            basewrapper.Base().info_logger(f"Error found, possibly no account found:  {e}")"""
+            basewrapper.Base().info_logger(f"Error found, possibly no account found:  {e}")
 
     @commands.command(pass_context=True)
     @commands.has_role("root")
