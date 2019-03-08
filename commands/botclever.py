eab327c7bbc46895fc01b41d6cbf68bb3983269c
IvanTurbin <ivan.turbin@gmail.com>
2019-03-08 04:53:43 +0200

    removed convo

--- commands/botclever.py
+++ commands/botclever.py
@@ -20,20 +20,6 @@ class Clever(object):
         self.clever.reset()
         return msg
 
-
-    @commands.command(pass_context=True)
-    async def clever_convo(self, ctx: commands.Context, *, message: str):
-        #self.clever.conversation()
-        data = {
-                "user": "PfIGLHA3k5dCcmd3",
-                "key": "N3BYYM9xcW547KC7dz7uvXGsR2JuZPps",
-                "nick": "ivan.turbin",
-                "text": message
-                }
-        r = requests.post(r"https://cleverbot.io/1.0/ask", data)
-        print(r.json())
-
-
     @commands.command(pass_context=True)
     async def clever(self, ctx: commands.Context, *, message: str):
         await self.client.send_typing(ctx.message.channel)
