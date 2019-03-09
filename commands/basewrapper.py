0af18faa96cb27a09cd53f4bcb5eec191268d635
IvanTurbin <ivan.turbin@gmail.com>
2019-03-09 05:03:51 +0200

    Establishing database connection for suggestions

--- commands/basewrapper.py
+++ commands/basewrapper.py
@@ -1,7 +1,6 @@
 from datetime import datetime
-import pyodbc
+import mysql.connector
 import json
-import configparser
 import os
 
 class Base(object):
@@ -18,34 +17,36 @@ class Base(object):
         data = {f"{author}": {"name": f"{author}", "playlist_link": f"{playlistlink}"}}
         return data
 
-    def get_token(self, title, token):
-        config = configparser.ConfigParser()
-        config.read(CFG_PATH)
-        return config[title][token]
-
     def get_config_vars(self, var):
         token = os.environ.get(var)
         return token
 
 class Database(object):
     def __init__(self):
+        Base().info_logger("PREPARING SQL CONNECTION")
         serverip = Base().get_config_vars("D_IP")
         databasename = Base().get_config_vars("D_DATABASE")
         password = Base().get_config_vars("D_PASSWORD")
         uid = Base().get_config_vars("D_USERNAME")
-        self.cnxn = pyodbc.connect('Driver={SQL Server};'
-                                f'Server={serverip};'
-                                f'Database={databasename};'
-                                f'PWD={password};'
-                                f'UID={uid};')
+        self.sql = mysql.connector.connect(host=serverip, database=databasename, user=uid, password=password)
 
     def write_suggestion(self, text):
+        Base().info_logger("SQL - Write suggestion")
         sql_code = f'INSERT INTO `suggestion` (`suggestions`) VALUES ({Base.info_logger(text)});'
-        self.cursor = self.cnxn.cursor()
-        self.cursor.execute(sql_code)
-        self.cursor.commit()
-        self.cursor.close()
-        self.cnxn.close()
+        try:
+            self.cursor = self.sql.cursor()
+            self.cursor.execute(sql_code)
+            self.cursor.commit()
+
+        except mysql.connector.Error as error:
+            self.sql.rollback()  # rollback if any exception occured
+            Base().info_logger("Failed inserting record into python_users table {}".format(error))
+
+        finally:
+            if self.sql.is_connected():
+                self.cursor.close()
+                self.sql.close()
+                Base().info_logger("SQL CONNECTION COMPLETE")
 
 
 class Json(object):
