8faf4274c654606b39158404e3a88316af06578d
IvanTurbin <ivan.turbin@gmail.com>
2019-03-10 03:49:55 +0200

    the fucking " marks

--- commands/misc.py
+++ commands/misc.py
@@ -101,7 +101,7 @@ class Misc(object):
         print(info.values)
         print("{player_count}/{max_players} {server_name}".format(**info))
 
-        embed = discord.Embed(title="{info.values["server_name"]}", url = r"steam://connect/54.37.242.130:27015")
+        embed = discord.Embed(title=f'{info.values["server_name"]}', url = r"steam://connect/54.37.242.130:27015")
         embed.add_field(name=Players, value=f'{info.values["player_count"]} / {info.values["max_players"]}', inline=True)
         embed.add_field(name=Gamemode, value=f'{info.values["game"]}', inline=True)
         embed.add_field(name=Map, value=f'{info.values["map"]}', inline=True)
