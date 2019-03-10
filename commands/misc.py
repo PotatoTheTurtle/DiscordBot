1a38b711c4873061a285b69f53ce62e78d21578c
IvanTurbin <ivan.turbin@gmail.com>
2019-03-10 04:00:27 +0200

    removed field

--- commands/misc.py
+++ commands/misc.py
@@ -105,7 +105,6 @@ class Misc(object):
         embed.add_field(name='Players', value=f'{info.values["player_count"]} / {info.values["max_players"]}', inline=True)
         embed.add_field(name='Gamemode', value=f'{info.values["game"]}', inline=True)
         embed.add_field(name='Map', value=f'{info.values["map"]}', inline=True)
-        embed.set_footer(text="Join server! steam://connect/54.37.242.130:27015")
         await self.client.say(embed=embed)
 
     @commands.command(pass_context=True)
