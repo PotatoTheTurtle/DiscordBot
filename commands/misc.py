a21e54091f8248b3659252b402a44f398ebdf483
IvanTurbin <ivan.turbin@gmail.com>
2019-03-10 04:18:30 +0200

    added url env vars

--- commands/misc.py
+++ commands/misc.py
@@ -94,6 +94,7 @@ class Misc(object):
         """
         ip = basewrapper.Base().get_config_vars("GMOD_ADDRESS")
         port = basewrapper.Base().get_config_vars("GMOD_PORT")
+        url = basewrapper.Base().get_config_vars("GMOD_URL")
         address = (ip, int(port))
         info = None
         try:
@@ -103,11 +104,10 @@ class Misc(object):
         except valve.source.NoResponseError:
             print("Master server request timed out!")
 
-        embed = discord.Embed(title=f'{info.values["server_name"]}')
+        embed = discord.Embed(title=f'{info.values["server_name"]}', url=url)
         embed.add_field(name='Players', value=f'{info.values["player_count"]} / {info.values["max_players"]}', inline=True)
         embed.add_field(name='Gamemode', value=f'{info.values["game"]}', inline=True)
         embed.add_field(name='Map', value=f'{info.values["map"]}', inline=True)
-        embed.set_footer(text="Join server! steam://connect/54.37.242.130:27015")
         await self.client.say(embed=embed)
 
     @commands.command(pass_context=True)
