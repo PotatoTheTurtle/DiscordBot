44c60b80fa7d28a4e73dcd8f841b431c61df5fee
IvanTurbin <ivan.turbin@gmail.com>
2019-03-11 02:27:28 +0200

    made top stats logic

--- commands/misc.py
+++ commands/misc.py
@@ -118,23 +118,32 @@ class Misc(object):
     async def stats(self, ctx: commands.Context, *,player=None):
         data = basewrapper.Database().get_player_data()
         array = []
-        print(ctx.message.content)
-        print(player)
         if player is None:
             for info in data:
-                name = info[1]
-                if name == player:
-                    money = info[3]
-                    array.append(sorted(zip(money, name), reverse=True)[:10])
-                    print(array)
-                    return
+                name = (info[1], info[3])
+                array.append(name)
+
+            top = sorted(array, reverse=True)[:10]
+
+            embed = discord.Embed(title="Top 10 Richest people")
+            embed.set_author(name=f'1. {top[0][0]} - ${top[0][1]}')
+            embed.set_author(name=f'2. {top[1][0]} - ${top[1][1]}')
+            embed.set_author(name=f'3. {top[2][0]} - ${top[2][1]}')
+            embed.set_author(name=f'4. {top[3][0]} - ${top[3][1]}')
+            embed.set_author(name=f'5. {top[4][0]} - ${top[4][1]}')
+            embed.set_author(name=f'6. {top[5][0]} - ${top[5][1]}')
+            embed.set_author(name=f'7. {top[6][0]} - ${top[6][1]}')
+            embed.set_author(name=f'8. {top[7][0]} - ${top[7][1]}')
+            embed.set_author(name=f'9. {top[8][0]} - ${top[8][1]}')
+            embed.set_author(name=f'10. {top[9][0]} - ${top[9][1]}')
+            await self.client.say(embed=embed)
             return
         try:
             for info in data:
                 name = info[1]
                 if name == player:
                     money = info[3]
-                    await self.client.say(f"{ctx.message.author.mention} Stats for {name}: Balance {money}")
+                    await self.client.say(f"{ctx.message.author.mention} Stats for {name}: Balance ${money}")
                     return
                 else:
                     continue
