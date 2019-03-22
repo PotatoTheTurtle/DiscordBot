84ec6cf0f6aa780f6c67985eea427efdf3d8a686
IvanTurbin <ivan.turbin@gmail.com>
2019-03-22 15:00:04 +0200

    debug

--- commands/misc.py
+++ commands/misc.py
@@ -97,25 +97,24 @@ class Misc(object):
         url = basewrapper.Base().get_config_vars("GMOD_URL")
         address = (ip, int(port))
         info = None
+        #try:
         try:
-            try:
-                with valve.source.a2s.ServerQuerier(address) as server:
-                    info = server.info()
-                    print(server.players())
+            with valve.source.a2s.ServerQuerier(address) as server:
+                info = server.info()
+                print(server.players())
 
-            except valve.source.NoResponseError as e:
-                print(e)
-                print("Master server request timed out!")
+        except valve.source.NoResponseError as e:
+            print(e)
+            print("Master server request timed out!")
 
-            embed = discord.Embed(title=f'{info.values["server_name"]}')
-            embed.add_field(name='Players', value=f'{info.values["player_count"]} / {info.values["max_players"]}', inline=True)
-            embed.add_field(name='Gamemode', value=f'{info.values["game"]}', inline=True)
-            embed.add_field(name='Map', value=f'{info.values["map"]}', inline=True)
-            embed.set_footer(text=f"Join server! {url}")
-            await self.client.say(embed=embed)
-        except:
-            print(Exception)
-            await self.client.say(f"{ctx.message.author.mention} Server either down or restarting. Please try again later.")
+        embed = discord.Embed(title=f'{info.values["server_name"]}')
+        embed.add_field(name='Players', value=f'{info.values["player_count"]} / {info.values["max_players"]}', inline=True)
+        embed.add_field(name='Gamemode', value=f'{info.values["game"]}', inline=True)
+        embed.add_field(name='Map', value=f'{info.values["map"]}', inline=True)
+        embed.set_footer(text=f"Join server! {url}")
+        await self.client.say(embed=embed)
+        #except:
+            #await self.client.say(f"{ctx.message.author.mention} Server either down or restarting. Please try again later.")
 
     @commands.command(pass_context=True)
     async def stats(self, ctx: commands.Context, *,player=None):
